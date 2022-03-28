02f59ba46673a6f507d7610d2e8365b6c6a8b8fd
Aman <Aman@hp>
2022-03-28 14:17:00 +0530

    DhProject updated

---
+++ com.DhProject/src/test/java/com/DhProjectPactice/CreateLeadsTest.java
@@ -0,0 +1,12 @@
+package com.DhProjectPactice;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadsTest {
+
+	@Test
+	public void createTest()
+	{
+		System.out.println("Leads created");
+	}
+}
